5b4c1525002be0781f88b7e48c7456aba9d573b7
eko777e <replitserver445@gmail.com>
2026-01-18 00:33:23 +0400

    Update play.py

--- ShrutiMusic/utils/inline/play.py
+++ ShrutiMusic/utils/inline/play.py
@@ -89,8 +89,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="💬 sᴜᴘᴘᴏʀᴛ", url=SUPPORT_GROUP),
-            InlineKeyboardButton(text="📢 ᴄʜᴀɴɴᴇʟ", url=SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="👨‍🔧 ᴅəsᴛəᴋ", url=SUPPORT_GROUP),
+            InlineKeyboardButton(text="🔮 ʏᴇɴiʟiᴋʟəʀ", url=SUPPORT_CHANNEL),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
